8e038fc426840e02f830123a0dd43eea5bd33cc2
Roger Zaugg <roger.zaugg@sbl.ch>
2012-05-09 00:38:01 +0200

    jhj

---
+++ src/MainClass.java
@@ -0,0 +1,16 @@
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abchalloServerClients666");
+		System.out.println("test234555555");
+
+		System.out.println("ddddddddddddddddddddddddddddrrrrrrrrraaaa");
+
+
+	}
+
+}
