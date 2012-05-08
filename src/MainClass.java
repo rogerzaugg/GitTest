27c39d5bec5eca9561c5053eecf3f69b35d1fc0f
rogerzaugg <roger.zaugg@sbl.com>
2012-05-09 00:18:22 +0300

    Update src/MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,6 +6,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("halloServerClients");
+		
+		System.out.println("test3");
 
 	}
 
