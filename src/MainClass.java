af2d147abf3b929e6cf108d20f5c41428d3f2351
Roger Zaugg <roger.zaugg@sbl.ch>
2012-05-08 23:13:39 +0200

    client

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("halloServer");
+		System.out.println("halloServerClient");
 
 	}
 
