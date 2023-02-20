a6dd6f719f9dea769e51c43fd90cbacb06129928
rogerzaugg <roger.zaugg@sbl.ch>
2023-02-20 10:45:22 +0100

    fixed blabla TG-Task 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,6 +7,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("abchalloServerClients666");
 		System.out.println("abc");
+		System.out.println("Test Taiga");
 
 	}
 
